efc38678ca99c28dde34b57dfb174c8868f008d8
ritik chugh <vishuchugh49@gmail.com>
2019-12-03 20:57:48 +0530

    first Comit

---
+++ travel/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index,name="index"),
+    path('about/', views.about,name="about"),
+    path('contact/', views.contact,name='contact'),
+    path('destinations/',views.destinations,name='destinations'),
+
+]
+urlpatterns=urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ travel/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
+from .models import destination
+
+def index(request):
+
+
+    dests=destination.objects.all()
+
+    return render (request,"index.html",{'dests':dests})
+     
+def about(request):
+
+    return render (request,"about.html")
+
+def contact(request):
+    return render (request,"contact.html")
+
+def destinations(request):
+    dests=destination.objects.all()
+    return render (request, "destinations.html",{'dests':dests})
+
+# Create your views here.
